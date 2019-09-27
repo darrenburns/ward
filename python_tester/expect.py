3a09a49b894a04d881fd70ca4792dbb22a78a229
Darren Burns <darren.burns@fanduel.com>
2019-09-27 16:24:58 +0100

    Create a context manager for expecting exceptions

---
+++ python_tester/expect.py
@@ -0,0 +1,18 @@
+from typing import Type
+
+
+class raises:
+    def __init__(self, expected_ex_type: Type[Exception]):
+        self.expected_ex_type = expected_ex_type
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not self.expected_ex_type:
+            raise AssertionError(
+                f"Expected exception {self.expected_ex_type}, but {exc_type} was raised instead."
+            )
+        return True
+
+
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,28 +1,20 @@
-from python_tester.fixtures import Fixture, FixtureRegistry
-
-
-# Start constructing a tree of fixtures
-
-def grandchild_a():
-    return 1
-
-
-def child_b():
-    return 1
-
-
-def child_a(grandchild_a):
-    return grandchild_a + 1
-
-
-def parent(child_a, child_b):
-    return child_a + child_b + 1
-
-
-# End constructing a tree of fixtures
+from python_tester import expect
+from python_tester.fixtures import Fixture, FixtureExecutionError, FixtureRegistry
 
 
 def test_fixture_resolve_resolves_tree_correctly():
+    def grandchild_a():
+        return 1
+
+    def child_b():
+        return 1
+
+    def child_a(grandchild_a):
+        return grandchild_a + 1
+
+    def parent(child_a, child_b):
+        return child_a + child_b + 1
+
     grandchild_a_fix = Fixture(name="grandchild_a", fn=grandchild_a)
     child_a_fix = Fixture(name="child_a", fn=child_a)
     child_b_fix = Fixture(name="child_b", fn=child_b)
@@ -36,3 +28,17 @@ def test_fixture_resolve_resolves_tree_correctly():
     # Each of the fixtures add 1, so the final value returned
     # by the tree should be 4, since there are 4 fixtures.
     assert resolved_parent == 4
+
+
+def test_fixture_resolve_raises_FixtureExecutionError_when_fixture_cant_be_executed():
+    def i_raise_an_exception():
+        # A ZeroDivisionError is thrown by this fixture,
+        # but we expect that this results in a FixtureExecutionError
+        raise ZeroDivisionError()
+
+    fix = Fixture(name="fix_a", fn=i_raise_an_exception)
+    registry = FixtureRegistry()
+    registry.cache_fixtures([fix])
+
+    with expect.raises(FixtureExecutionError):
+        fix.resolve(registry)
