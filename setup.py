b75280b316de2ed8cabd0deedfafb10b6a572511
Darren Burns <darren.burns@fanduel.com>
2019-09-29 00:17:48 +0100

    Adding long description to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 version = "0.1.0a0"
 
 setup(
     name="ward",
     version=version,
     description="A Python 3 test framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="http://github.com/darrenburns/ward",
     author="Darren Burns",
     author_email="darrenb900@gmail.com",
     license="MIT",
     packages=["ward"],
     python_requires=">=3.6",
-    zip_safe=False,
 )
