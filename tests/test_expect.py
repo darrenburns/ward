085af666b7f16e2fa94162a305571b0c88d9e0fb
Darren Burns <darren.burns@fanduel.com>
2019-10-07 21:55:23 +0200

    Add a test for satisfies predicate

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -42,6 +42,25 @@ def test_equals_failure_history_recorded():
     expect(e.history).equals(hist)
 
 
+def test_satisfies_success_history_recorded():
+    this = "olleh"
+    predicate = lambda e: this[::-1] == "hello"
+    e = expect(this).satisfies(predicate)
+
+    hist = [
+        Expected(
+            this=this,
+            op="satisfies",
+            that=predicate,
+            op_args=(),
+            op_kwargs={},
+            success=True,
+        )
+    ]
+
+    expect(e.history).equals(hist)
+
+
 def test_approx_success_history_recorded():
     this, that, eps = 1.0, 1.01, 0.5
 
