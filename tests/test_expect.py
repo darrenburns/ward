0dd3b1d53c266ae081fcc65211cf5e520ada20e0
Darren Burns <darren.burns@fanduel.com>
2019-10-20 03:55:14 +0100

    Add more tests around expect API integration with mock

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -1,4 +1,7 @@
-from ward import expect
+from contextlib import suppress
+from unittest.mock import Mock
+
+from ward import expect, fixture
 from ward.expect import Expected, ExpectationFailed
 
 
@@ -15,10 +18,8 @@ def test_equals_failure_history_recorded():
     this, that = "hello", "goodbye"
 
     e = expect(this)
-    try:
+    with suppress(ExpectationFailed):
         e.equals(that)
-    except ExpectationFailed:
-        pass
 
     hist = [Expected(this=this, op="equals", that=that, op_args=(), op_kwargs={}, success=False)]
     expect(e.history).equals(hist)
@@ -39,10 +40,8 @@ def test_satisfies_failure_history_recorded():
     predicate = lambda e: False
 
     e = expect(this)
-    try:
+    with suppress(ExpectationFailed):
         e.satisfies(predicate)
-    except ExpectationFailed:
-        pass
 
     hist = [Expected(this=this, op="satisfies", that=predicate, op_args=(), op_kwargs={}, success=False)]
 
@@ -54,7 +53,8 @@ def test_approx_success_history_recorded():
 
     e = expect(this).approx(that, abs_tol=eps)
 
-    hist = [Expected(this=this, op="approx", that=that, op_args=(), op_kwargs={"abs_tol": eps}, success=True)]
+    hist = [Expected(this=this, op="approx", that=that, op_args=(), op_kwargs={"rel_tol": 1e-09, "abs_tol": 0.5},
+                     success=True)]
     expect(e.history).equals(hist)
 
 
@@ -63,11 +63,96 @@ def test_approx_failure_history_recorded():
 
     # This will raise an ExpectationFailed, which would fail this test unless we catch it.
     e = expect(this)
-    try:
+    with suppress(ExpectationFailed):
         e.approx(that, eps)
-    except ExpectationFailed:
-        pass
 
-    hist = [Expected(this=this, op="approx", that=that, op_args=(eps,), op_kwargs={}, success=False)]
+    hist = [Expected(this=this, op="approx", that=that, op_args=(), op_kwargs={"rel_tol": 0.001, "abs_tol": 0.0},
+                     success=False)]
 
+    expect(e.history).equals(hist)
+
+
+def test_not_equals_success_history_recorded():
+    this, that = 1, 2
+
+    e = expect(this).not_equals(that)
+
+    hist = [Expected(this, op="not_equals", that=that, op_args=(), op_kwargs={}, success=True)]
+    expect(e.history).equals(hist)
+
+
+def test_not_equals_failure_history_recorded():
+    this, that = 1, 1
+
+    e = expect(this)
+    with suppress(ExpectationFailed):
+        e.not_equals(that)
+
+    hist = [Expected(this, op="not_equals", that=that, op_args=(), op_kwargs={}, success=False)]
+    expect(e.history).equals(hist)
+
+
+@fixture
+def mock():
+    return Mock()
+
+
+def test_mock_called_success(mock):
+    mock()
+    e = expect(mock).called()
+
+    hist = [Expected(mock, op="called", that=None, op_args=(), op_kwargs={}, success=True)]
+    expect(e.history).equals(hist)
+
+
+def test_mock_called_failure(mock):
+    e = expect(mock)
+    with suppress(ExpectationFailed):
+        e.called()
+
+    hist = [Expected(mock, op="called", that=None, op_args=(), op_kwargs={}, success=False)]
+    expect(e.history).equals(hist)
+
+
+def test_mock_not_called_success(mock):
+    e = expect(mock).not_called()
+
+    hist = [Expected(mock, op="not_called", that=None, op_args=(), op_kwargs={}, success=True)]
+    expect(e.history).equals(hist)
+
+
+def test_mock_called_once_with_success(mock):
+    args = (1, 2, 3)
+    kwargs = {"hello": "world"}
+    mock(*args, **kwargs)
+
+    e = expect(mock).called_once_with(*args, **kwargs)
+
+    hist = [Expected(mock, op="called_once_with", that=None, op_args=args, op_kwargs=kwargs, success=True)]
+    expect(e.history).equals(hist)
+
+
+def test_mock_called_once_with_failure_missing_arg(mock):
+    args = (1, 2, 3)
+    kwargs = {"hello": "world"}
+    mock(1, 2, **kwargs)  # 3 is missing intentionally
+
+    e = expect(mock)
+    with suppress(ExpectationFailed):
+        e.called_once_with(*args, **kwargs)
+
+    hist = [Expected(mock, op="called_once_with", that=None, op_args=args, op_kwargs=kwargs, success=False)]
+    expect(e.history).equals(hist)
+
+
+def test_mock_called_once_with_failure_missing_kwarg(mock):
+    args = (1, 2, 3)
+    kwargs = {"hello": "world"}
+    mock(*args, wrong="thing")
+
+    e = expect(mock)
+    with suppress(ExpectationFailed):
+        e.called_once_with(*args, **kwargs)
+
+    hist = [Expected(mock, op="called_once_with", that=None, op_args=args, op_kwargs=kwargs, success=False)]
     expect(e.history).equals(hist)
--- ward/expect.py
+++ ward/expect.py
@@ -1,7 +1,9 @@
 import functools
+import inspect
 import math
 from dataclasses import dataclass
-from typing import Type, Any, List, Callable, Dict, Tuple
+from typing import Type, Any, List, Callable, Dict, Tuple, Optional
+from unittest.mock import _Call
 
 
 class raises:
@@ -21,7 +23,7 @@ class raises:
 class Expected:
     this: Any
     op: str
-    that: Any
+    that: Optional[Any]
     op_args: Tuple
     op_kwargs: Dict
     success: bool = True
@@ -56,108 +58,142 @@ class expect:
         self.this = this
         self.history: List[Expected] = []
 
-    @record_and_handle_outcome
-    def equals(self, that: Any):
-        return self.this == that
+    def equals(self, expected: Any):
+        return self._handle_expect(self.this == expected, that=expected)
 
     def not_equals(self, that: Any):
-        return not self.equals(that)
+        return self._handle_expect(self.this != that, that=that)
 
-    @record_and_handle_outcome
     def less_than(self, that: Any):
-        return self.this < that
+        return self._handle_expect(self.this < that, that=that)
 
     def not_less_than(self, that: Any):
-        return not self.less_than(that)
+        return self._handle_expect(not self.this < that, that=that)
 
-    @record_and_handle_outcome
     def less_than_or_equals(self, that: Any):
-        return self.this <= that
+        return self._handle_expect(self.this <= that, that=that)
 
     def not_less_than_or_equals(self, that: Any):
-        return self.less_than_or_equals(that)
+        return self._handle_expect(not self.this <= that, that=that)
 
-    @record_and_handle_outcome
     def greater_than(self, that: Any):
-        return self.this > that
+        return self._handle_expect(self.this > that, that=that)
 
     def not_greater_than(self, that: Any):
-        return not self.greater_than(that)
+        return self._handle_expect(not self.this > that, that=that)
 
-    @record_and_handle_outcome
     def greater_than_or_equals(self, that: Any):
-        return self.this >= that
+        return self._handle_expect(self.this >= that, that=that)
 
     def not_greater_than_or_equals(self, that: Any):
-        return not self.greater_than_or_equals(that)
+        return self._handle_expect(not self.this >= that, that=that)
 
-    @record_and_handle_outcome
     def contains(self, that: Any):
-        return that in self.this
+        return self._handle_expect(that in self.this, that=that)
 
     def not_contains(self, that: Any):
-        return not self.contains(that)
+        return self._handle_expect(that not in self.this, that=that)
 
-    @record_and_handle_outcome
     def has_length(self, length: int):
-        return len(self.this) == length
+        return self._handle_expect(len(self.this) == length, that=length)
 
     def not_has_length(self, length: int):
-        return not self.has_length(length)
+        return self._handle_expect(len(self.this) != length, that=length)
 
-    @record_and_handle_outcome
     def instance_of(self, type: Type):
-        return isinstance(self.this, type)
+        return self._handle_expect(isinstance(self.this, type), that=type)
 
     def not_instance_of(self, type: Type):
-        return not self.instance_of(type)
+        return self._handle_expect(not isinstance(self.this, type), that=type)
 
-    @record_and_handle_outcome
     def satisfies(self, predicate: Callable[["expect"], bool]):
-        return predicate(self.this)
+        return self._handle_expect(predicate(self.this), that=predicate)
 
     def not_satisfies(self, predicate: Callable[["expect"], bool]):
-        return not self.satisfies(predicate)
+        return self._handle_expect(not predicate(self.this), that=predicate)
 
-    @record_and_handle_outcome
     def identical_to(self, that: Any):
-        return self.this is that
+        return self._handle_expect(self.this is that, that=that)
 
     def not_identical_to(self, that: Any):
-        return not self.identical_to(that)
+        return self._handle_expect(not self.identical_to(that), that=that)
 
-    @record_and_handle_outcome
     def approx(self, that: Any, rel_tol: float = 1e-9, abs_tol: float = 0.0):
-        return math.isclose(self.this, that, abs_tol=abs_tol, rel_tol=rel_tol)
+        return self._handle_expect(
+            math.isclose(self.this, that, abs_tol=abs_tol, rel_tol=rel_tol),
+            that=that,
+            rel_tol=rel_tol,
+            abs_tol=abs_tol,
+        )
 
     def not_approx(self, that: Any, rel_tol: float = 1e-9, abs_tol: float = 0.0):
-        return not self.approx(that, rel_tol=rel_tol, abs_tol=abs_tol)
+        return self._handle_expect(
+            not math.isclose(self.this, that, abs_tol=abs_tol, rel_tol=rel_tol),
+            that=that,
+            rel_tol=rel_tol,
+            abs_tol=abs_tol,
+        )
 
-    @record_and_handle_outcome
     def called(self):
-        return self.this.called
+        return self._handle_expect(self.this.called)
 
     def not_called(self):
-        return not self.called
+        return self._handle_expect(not self.this.called)
 
-    @record_and_handle_outcome
     def called_once_with(self, *args, **kwargs):
         try:
             self.this.assert_called_once_with(*args, **kwargs)
+            passed = True
         except AssertionError:
-            return False
-        return True
+            passed = False
+        print(args, kwargs)
+        return self._handle_expect(passed, *args, **kwargs)
 
-    def not_called_once_with(self, *args, **kwargs):
-        return not self.called_once_with(*args, **kwargs)
-
-    @record_and_handle_outcome
     def called_with(self, *args, **kwargs):
         try:
             self.this.assert_called_with(*args, **kwargs)
+            passed = True
         except AssertionError:
-            return False
-        return True
+            passed = False
+        return self._handle_expect(passed, *args, **kwargs)
 
-    def not_called_with(self, *args, **kwargs):
-        return not self.called_with(*args, **kwargs)
+    def has_calls(self, calls: List[_Call], any_order: bool = False):
+        try:
+            self.this.assert_has_calls(calls, any_order=any_order)
+            passed = True
+        except AssertionError:
+            passed = False
+        return self._handle_expect(
+            passed,
+            calls=calls,
+            any_order=any_order,
+        )
+
+    def _store_in_history(
+        self, result: bool, called_with_args: Tuple[Any], called_with_kwargs: Dict[str, Any], that=None
+    ) -> bool:
+        self.history.append(
+            Expected(
+                this=self.this,
+                op=inspect.stack()[2].function,  # :)
+                that=that,
+                success=result,
+                op_args=called_with_args,
+                op_kwargs=called_with_kwargs,
+            )
+        )
+        return result
+
+    def _fail_if_false(self, val: bool) -> bool:
+        if val:
+            return True
+        raise ExpectationFailed("expectation failed", self.history)
+
+    def _handle_expect(
+        self, result: bool, *args, that: Any = None, **kwargs
+    ) -> "expect":
+        self._store_in_history(
+            result, that=that, called_with_args=args, called_with_kwargs=kwargs
+        )
+        self._fail_if_false(result)
+        return self
