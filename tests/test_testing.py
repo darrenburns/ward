a882991ce892d2b6a2b0092d05c994de3fd5556e
Darren Burns <darren.burns@fanduel.com>
2019-10-25 00:11:11 +0100

    Ensuring Test.__call__ delegates as expected

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from ward import test, expect, fixture
 from ward.fixtures import FixtureRegistry, Fixture
 from ward.testing import Test
@@ -55,7 +57,6 @@ def _(anonymous_test):
 def _(anonymous_test):
     expect(anonymous_test.deps()).equals({})
 
-
 @test("Test.deps should return correct params when test uses fixtures")
 def _(dependent_test):
     deps = dependent_test.deps()
@@ -87,3 +88,11 @@ def _():
 
     expect(test.resolve_args(reg)).equals({"my_fixture": fixture})
     expect(fixture.resolved_val).equals(val)
+
+@test("Test.__call__ should delegate to the function it wraps")
+def _():
+    mock = Mock()
+    t = Test(fn=mock, module_name=mod)
+    t(1, 2, key="val")
+    expect(mock).called_once_with(1, 2, key="val")
+
