7fe4da34b5247a36dfc49321740a4048233dcb1d
Darren Burns <darren.burns@fanduel.com>
2019-10-30 18:33:56 +0000

    Rename a test

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -10,7 +10,7 @@ def exception_raising_fixture():
     return Fixture(fn=i_raise_an_exception)
 
 
-@test("FixtureRegistry.cache_fixture can store and retrieve a single fixture")
+@test("FixtureCache.cache_fixture can store and retrieve a single fixture")
 def _(f=exception_raising_fixture):
     cache = FixtureCache()
     cache.cache_fixture(f)
