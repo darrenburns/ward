e3d9eb368c9a78de6fb95c84e73061e2c938206a
Darren Burns <darren.burns@fanduel.com>
2019-12-14 16:33:43 +0000

    Add option to print dot per test

--- ward/util.py
+++ ward/util.py
@@ -27,3 +27,13 @@ def get_exit_code(results: Iterable[TestResult]) -> ExitCode:
 def truncate(s: str, num_chars: int) -> str:
     suffix = "..." if len(s) > num_chars else ""
     return s[:num_chars - len(suffix)] + suffix
+
+
+def outcome_to_colour(outcome: TestOutcome) -> str:
+    return {
+        TestOutcome.PASS: "green",
+        TestOutcome.SKIP: "blue",
+        TestOutcome.FAIL: "red",
+        TestOutcome.XFAIL: "magenta",
+        TestOutcome.XPASS: "yellow",
+    }[outcome]
