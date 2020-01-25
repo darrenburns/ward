c2b3c50bbe806c297dcce2e1462c53e79e47c9d9
Darren Burns <darren.burns@fanduel.com>
2020-01-25 15:48:16 +0000

    Testing that assert x == y is rewritten correctly

--- tests/test_rewrite.py
+++ tests/test_rewrite.py
@@ -1,18 +1,20 @@
+import ast
+
 from tests.test_suite import testable_test
 from ward import test, fixture, raises
 from ward.expect import TestFailure
-from ward.rewrite import assert_equal, rewrite_assertions_in_tests
+from ward.rewrite import assert_equal, rewrite_assertions_in_tests, RewriteAssert
 from ward.testing import Test
 
 
 @testable_test
 def passing_fn():
-    assert True
+    assert 1 == 1
 
 
 @testable_test
 def failing_fn():
-    assert False
+    assert 1 == 2
 
 
 @fixture
@@ -47,3 +49,27 @@ def _(p=passing, f=failing):
     assert [(test.description, test.id, test.module_name) for test in in_tests] == [
         (test.description, test.id, test.module_name) for test in out_tests
     ]
+
+
+@test("RewriteAssert.visit_Assert doesn't touch `assert x`")
+def _():
+    src = "assert False"
+    in_tree = ast.parse(src).body[0]
+    out_tree = RewriteAssert().visit(in_tree)
+    assert in_tree == out_tree
+
+
+@test("RewriteAssert.visit_Assert transforms `assert x == y`")
+def _():
+    src = "assert 1 == 2"
+    in_tree = ast.parse(src).body[0]
+    out_tree = RewriteAssert().visit(in_tree)
+
+    assert out_tree.lineno == in_tree.lineno
+    assert out_tree.col_offset == in_tree.col_offset
+    assert out_tree.value.lineno == in_tree.lineno
+    assert out_tree.value.col_offset == in_tree.col_offset
+    assert out_tree.value.func.id == "assert_equal"
+    assert out_tree.value.args[0].n == 1
+    assert out_tree.value.args[1].n == 2
+    assert out_tree.value.args[2].s == ''
