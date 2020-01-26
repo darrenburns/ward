91b483555a20642c86a86c606764ca03758df8a8
Darren Burns <darren.burns@fanduel.com>
2020-01-26 03:02:35 +0000

    Remove unused kwarg from build_unified_diff

--- ward/diff.py
+++ ward/diff.py
@@ -28,7 +28,7 @@ def bright_green(s: str) -> str:
     return f"{Fore.LIGHTGREEN_EX}{s}{Style.RESET_ALL}"
 
 
-def build_unified_diff(lhs_repr, rhs_repr, margin_left=4) -> str:
+def build_unified_diff(lhs_repr, rhs_repr) -> str:
     differ = difflib.Differ()
     lines_lhs = lhs_repr.splitlines()
     lines_rhs = rhs_repr.splitlines()
