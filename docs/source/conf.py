307fc73de0bf6015e4bafe3f337027fe19cfd6e0
Darren Burns <darrenb900@gmail.com>
2021-04-02 03:47:29 +0100

    Prepare 0.53.0b0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Darren Burns'
 author = 'Darren Burns'
 
 # The full version, including alpha/beta/rc tags
-release = '0.52.1b0'
+release = '0.53.0b0'
 
 
 # -- General configuration ---------------------------------------------------
