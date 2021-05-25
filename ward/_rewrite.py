0d7909017c19b77ce18fd67ddb20e4690bd35243
Darren Burns <darrenb900@gmail.com>
2021-05-25 20:13:08 +0100

    Remove a comment

--- ward/_rewrite.py
+++ ward/_rewrite.py
@@ -62,7 +62,6 @@ def is_comparison_type(node: ast.expr, type) -> bool:
 
 
 class RewriteAssert(ast.NodeTransformer):
-    # TODO: mention it in comments to PR
     def visit_Assert(self, node):  # noqa: C901 - no chance to reduce complexity
         if is_binary_comparison(node):
             if is_comparison_type(node, ast.Eq):
